a7762a9c5461bb38c7406ef4dac3d99cecb61e0c
leoJ07 <joel@achievers.se>
2020-09-26 10:39:29 +0000

    express

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
-const http = require("http");
+const express = require("express");
+const fs = require("fs");
+const app = express();
 
-http.createServer((req, res) => {
-    res.end("hello");
-    console.log("yay");
-}).listen(3000);
+app.get("/", (req, res) => {
+    fs.readFile("./site/index.html", (err, data) => {
+        if(err){
+            console.log(err);
+            res.send();
+        } else {
+            res.send(data.toString());
+        }
+    });
+});
+
+
+app.get("/register", (req, res) => {
+    fs.readFile("./site/reg.html", (err, data) => {
+        if(err){
+            console.log(err);
+            res.send();
+        } else {
+            res.send(data.toString());
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log("server is runing");
+});
